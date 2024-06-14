55597c00dfcbadd1bbd4c0d66ed467a216590372
Stephen Aranda <aranda.stephen88@gmail.com>
2024-06-13 20:29:28 -0500

    installed thunk, logger. completed store

---
+++ state/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "../reducers/RootReducer";
+import logger from 'redux-logger'
+import { thunk } from 'redux-thunk'
+
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger)
+
+})

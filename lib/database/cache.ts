d1e8d87cb191b405456e777576af09767a99edab
Juan de Urtubey <juan@develophealth.io>
2024-07-01 11:56:54 -0500

    disable caching if env vars not set

--- lib/database/cache.ts
+++ lib/database/cache.ts
@@ -9,6 +9,11 @@ export const getCachedAutofillResult = async (
   kind: "current" | "forecast",
   word: string
 ): Promise<AutofillResponse | null> => {
+  if (!process.env["KV_URL"]) {
+    // eslint-disable-next-line unicorn/no-null
+    return null;
+  }
+
   const key = getAutofillCacheKey(kind, word);
   return kv.get<AutofillResponse>(key);
 };
@@ -18,6 +23,10 @@ export const setCachedAutofillResult = async (
   word: string,
   result: AutofillResponse
 ): Promise<void> => {
+  if (!process.env["KV_URL"]) {
+    return;
+  }
+
   const key = getAutofillCacheKey(kind, word);
   await kv.set(key, result, {
     ex: 60 * 60 * 24 * 7, // 1 week (in seconds)

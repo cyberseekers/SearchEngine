3f106c15ea76ba6e1032526638f4edede499a87a
Stephen Aranda <aranda.stephen88@gmail.com>
2024-07-19 18:25:30 -0500

    created link for home

--- components/logged-out-nav.jsx
+++ components/logged-out-nav.jsx
@@ -1,17 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 
-const loggedOutNav = () => {
+const LoggedOutNav = () => {
+
+
 
     return (
         <div className="nav">
-
-
-
-
+            <Link href='/'>Home</Link>
         </div>
 
     )
 }
 
-export default loggedOutNav
+export default LoggedOutNav

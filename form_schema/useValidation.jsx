10cda380dc28d7270f7f7094aa3e5162187bf2b4
Stephen Aranda <aranda.stephen88@gmail.com>
2024-07-24 06:59:59 -0500

    used yup to set errors

--- form_schema/useValidation.jsx
+++ form_schema/useValidation.jsx
@@ -14,4 +14,19 @@ const initialErrors = {
 export const useValidation = (schema) => {
     const [data, setData] = useState(initialValues)
     const [errors, setErrors] = useState(initialErrors)
+
+    const onInputChange = (event) => {
+        const { name, value } = event.target
+        yup.reach(schema, name)
+            .validation(value)
+            .then(() => {
+                setErrors({ ...errors, [name]: "" })
+            })
+            .catch(error => {
+                setErrors({ ...errors, [name]: error.errors[0] })
+
+            })
+
+
+    }
 }

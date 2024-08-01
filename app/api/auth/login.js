be58bf762317d9e214d48c78267df6e1cb5299b9
Juan de Urtubey <juan@develophealth.io>
2024-07-31 22:53:31 -0500

    add hook for persisting user details

--- app/api/auth/login.js
+++ app/api/auth/login.js
@@ -1,29 +1,28 @@
-import { prisma } from '../../../lib/database/index'
-import crypto from 'node:crypto';
-
-
+import { prisma } from "../../../lib/database/index";
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { username, password } = req.body;
+  if (req.method === "POST") {
+    const { username, password } = req.body;
 
-        try {
-            const user = await prisma.user.findUnique({
-                where: { username },
-            });
+    try {
+      const user = await prisma.user.findUnique({
+        where: { username },
+        include: { Admin: true },
+      });
 
-            if (user && user.passwordHash === password) {
-                const token = crypto.randomBytes(16).toString('hex');
-                res.status(200).json({ token });
-            } else {
-                res.status(401).json({ message: 'Invalid username or password' });
-            }
-        } catch (error) {
-            console.error('Error during login:', error);
-            res.status(500).json({ message: 'Internal server error' });
-        }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} not allowed`);
+      if (user && user.passwordHash === password) {
+        res
+          .status(200)
+          .json({ username: user.username, isAdmin: !!user.Admin });
+      } else {
+        res.status(401).json({ message: "Invalid username or password" });
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).json({ message: "Internal server error" });
     }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} not allowed`);
+  }
 }
